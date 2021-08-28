8b8304805b15c9861d4c23b74daa67a0a7adb939
aye84 <andrew.ye.us@gmail.com>
2021-08-28 11:45:29 -0500

    First Files

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$('nav li').hover(function() {
+  $('ul', this).stop().slideDown(200);
+},
+function() {
+  $('ul', this).stop() .slideUp(200);
+})
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
