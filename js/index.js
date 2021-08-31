85fb1b2f37ec82c5069f713f034dfefadbc617a3
aye84 <andrew.ye.us@gmail.com>
2021-08-30 20:41:30 -0500

    v3

--- js/index.js
+++ js/index.js
@@ -20,3 +20,8 @@ window.onclick = function(event) {
     }
   }
 }
+$(".jumbotron").css({ height: $(window).height() + "px" });
+
+$(window).on("resize", function() {
+  $(".jumbotron").css({ height: $(window).height() + "px" });
+});
